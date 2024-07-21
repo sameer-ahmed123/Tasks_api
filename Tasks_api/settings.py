1a11dff282eee85e48b094efb1185f6eb26528a6
sameer <sameerkhanjan123@gmail.com>
2024-07-21 21:17:18 +0500

    shift to railway

--- Tasks_api/settings.py
+++ Tasks_api/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'Tasks_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
+        'USER':os.environ.get('POSTGRES_USER'),
+        'PASSWORD':os.environ.get('POSTGRES_PASSWORD'),
+        'HOST':os.environ.get('POSTGRES_HOST'),
+        'PORT':os.environ.get('POSTGRES_PORT')
     }
 }
+
 
 
 # Password validation
