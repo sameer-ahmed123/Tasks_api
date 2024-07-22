fd51c9a5bcb119f73c1cd2a99f9331118dbc98d2
sameer <sameerkhanjan123@gmail.com>
2024-07-22 09:24:51 +0500

    deploy

--- Tasks_api/settings.py
+++ Tasks_api/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-qg-u)ggkh-b01-4+eroa#ljt=dz@ykn_w#z839arapy&!%0(e)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","tasksapi-production-a932.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ["https://tasksapi-production-a932.up.railway.app"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","tasksapi-production-9477.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://tasksapi-production-9477.up.railway.app"]
 
 # Application definition
 
